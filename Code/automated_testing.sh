5b5d26614fb15153f3c57a05a5b363a0107c4e98
Ken Su <kensu324@gmail.com>
2024-09-10 14:12:02 -0400

    added bash file

---
+++ Code/automated_testing.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Path to your virtual environment's activate script
+VENV_PATH="buffet_venv/bin/activate"
+
+# Directory where the code is located
+CODE_DIR="/path/to/Code"
+
+# Path to the Python file you want to execute
+PYTHON_FILE="Buffet/Code/paper_trading.py"
+
+# Activate the virtual environment
+source "$VENV_PATH"
+
+# Navigate to the Code directory
+cd "$CODE_DIR" || exit
+
+# Run the Python script with nohup to keep it running in the background
+nohup python -u "$PYTHON_FILE" &
+
+echo "Script is running in the background. Check output.log for details."
